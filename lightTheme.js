9304a738eb9c8e9c7c667c9fb3576c10ed4e57fa
archhaze24 <archhaze24@gmail.com>
2022-06-20 22:52:03 +0300

    Changed an entry for 2022-06-20, added light theme toggle.

---
+++ lightTheme.js
@@ -0,0 +1,5 @@
+function toggleLightTheme() {
+    document.body.classList.toggle("lightTheme");
+    document.getElementById("title").classList.toggle("lightThemeTitle")
+    document.getElementById("lightThemeIcon").classList.toggle("lightThemeIconDark")
+}
